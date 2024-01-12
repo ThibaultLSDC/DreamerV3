added kl, fixed straight through gradients

DreamerV3/models/dists.py
@@ -1,5 +1,6 @@
 import torch
 from torch.distributions import Independent, OneHotCategorical
+from torch.distributions.kl import kl_divergence
 
 
 class ReshapeCategorical:
@@ -9,8 +10,8 @@ class ReshapeCategorical:
         self.dist = Independent(OneHotCategorical(logits=self.logits), 1)
 
     def sample(self):
-        sample = self.dist.sample()
-        probs = self.dist.probs
+        sample = self.dist.sample().reshape(self.logits.shape[0], -1)
+        probs = self.dist.base_dist.probs.reshape(self.logits.shape[0], -1)
         return sample + probs - probs.detach()
     
     def log_prob(self, x):
@@ -18,3 +19,6 @@ class ReshapeCategorical:
     
     def entropy(self):
         return self.dist.entropy()
+
+    def kl(self, other):
+        return kl_divergence(self.dist, other.dist)